fa802dde5c73e6fa42dbfe6f12b1ed8a639873c7
임동희 <n_cooldong@naver.com>
2021-06-10 17:34:51 +0900

    Create template_literals.js

---
+++ WEB2_nodejs/syntax/template_literals.js
@@ -0,0 +1,9 @@
+var name = "LEE"
+var string = 'I am great ' +name+' \
+\
+that buys everything in the world.';
+console.log(string);
+var string = `I am great ${name}
+
+that buys every${1+2}thing in the world.`;
+console.log(string);
